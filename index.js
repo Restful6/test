7b87a47234395a35aa8df8316612b68b7221e8a5
Restful6 <145867685+Restful6@users.noreply.github.com>
2025-04-13 14:55:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { OpenAI } = require("openai");
 
 const openai = new OpenAI({
-  apiKey: "YOUR_OPENAI_API_KEY", // ή από env μεταβλητή
+  apiKey: "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", // ή από env μεταβλητή
 });
 
 app.post("/chat", async (req, res) => {
